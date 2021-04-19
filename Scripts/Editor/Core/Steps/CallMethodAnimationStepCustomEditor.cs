b7550cc94540dec6dfc9990c8cc4d988d99251c2
Vladmir Souza <vladmir.souza@miniclip.com>
2021-04-19 16:53:37 +0100

    Add call method animation step

---
+++ Scripts/Editor/Core/Steps/CallMethodAnimationStepCustomEditor.cs
@@ -0,0 +1,31 @@
+﻿using BrunoMikoski.AnimationSequencer;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(CallMethodAnimationStep))]
+public class CallMethodAnimationStepCustomEditor : AnimationStepBasePropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        DrawBaseGUI(position, property, label);
+        
+        float originY = position.y;
+
+        position.height = EditorGUIUtility.singleLineHeight;
+
+        if (property.isExpanded)
+        {
+            EditorGUI.indentLevel++;
+            position = EditorGUI.IndentedRect(position);
+            EditorGUI.indentLevel--;
+            
+            position.height = EditorGUIUtility.singleLineHeight;
+            position.y += base.GetPropertyHeight(property, label)+ EditorGUIUtility.standardVerticalSpacing;
+            SerializedProperty methodToCallSerializedProperty = property.FindPropertyRelative("methodToCall");
+            float propertyHeight = EditorGUI.GetPropertyHeight(methodToCallSerializedProperty, label);
+            position.y += propertyHeight;
+        }
+        
+        property.SetPropertyDrawerHeight(position.y - originY + EditorGUIUtility.singleLineHeight);
+    }
+}
---
+++ Scripts/Runtime/Core/Steps/CallMethodAnimationStep.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace BrunoMikoski.AnimationSequencer
+{
+    public class CallMethodAnimationStep : AnimationStepBase
+    {
+        [SerializeField]
+        private UnityEvent methodToCall;
+        
+        public override string DisplayName => "Method Call Step";
+        
+        public override float Duration { get; }
+
+        public override bool CanBePlayed() => true;
+        
+        public override string GetDisplayNameForEditor(int index)
+        {
+            string targetName = "NULL";
+            if (methodToCall != null)
+                targetName = methodToCall.GetPersistentTarget(0).name;
+
+            string persistentTargetNames = String.Empty;
+            for (int i = 0; i < methodToCall.GetPersistentEventCount(); i++)
+            {
+                persistentTargetNames = $"{String.Join(", ", methodToCall.GetPersistentTarget(i).name).Truncate(45)}";
+            }
+            
+            return $"{index}. {DisplayName}: {persistentTargetNames}";
+        }
+
+        public override void Complete()
+        {
+            methodToCall?.Invoke();
+        }
+    }
+}
