819a4853b043bb7632414c1913d1e32841649100
Bruno Mikoski <bruno.mikoski@bossastudios.com>
2023-07-08 10:42:59 +0100

    add: default TEXT

--- Scripts/Runtime/Extensions/DOTweenExtensions.cs
+++ Scripts/Runtime/Extensions/DOTweenExtensions.cs
@@ -3,6 +3,7 @@ using DG.Tweening;
 using DG.Tweening.Core;
 using DG.Tweening.Plugins.Options;
 using TMPro;
+using UnityEngine.UI;
 
 namespace BrunoMikoski.AnimationSequencer
 {
@@ -15,10 +16,21 @@ namespace BrunoMikoski.AnimationSequencer
                 endValue = "";
             }
             TweenerCore<string, string, StringOptions> t = DOTween.To(() => target.text, x => target.text = x, endValue, duration);
-            t.SetOptions(richTextEnabled, scrambleMode, scrambleChars)
-                .SetTarget(target);
+            t.SetOptions(richTextEnabled, scrambleMode, scrambleChars).SetTarget(target);
             return t;
         }
+        
+        public static TweenerCore<string, string, StringOptions> DOText(this Text target, string endValue, float duration, bool richTextEnabled = true, ScrambleMode scrambleMode = ScrambleMode.None, string scrambleChars = null)
+        {
+            if (endValue == null) {
+                if (Debugger.logPriority > 0) Debugger.LogWarning("You can't pass a NULL string to DOText: an empty string will be used instead to avoid errors");
+                endValue = "";
+            }
+            TweenerCore<string, string, StringOptions> t = DOTween.To(() => target.text, x => target.text = x, endValue, duration);
+            t.SetOptions(richTextEnabled, scrambleMode, scrambleChars).SetTarget(target);
+            return t;
+        }
+
     }
 }
 #endif
