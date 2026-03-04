6521ce5f6286158c716c0ac16523ba6bea33f08f
Muldoon, Niall, HL <Niall.Muldoon@sega.co.uk>
2026-03-04 17:25:17 +0000

    - Whitespace tidied up.

--- Scripts/Runtime/Core/Steps/SetGameObjectActiveStep.cs
+++ Scripts/Runtime/Core/Steps/SetGameObjectActiveStep.cs
@@ -31,7 +31,6 @@ namespace BrunoMikoski.AnimationSequencer
         public override void AddTweenToSequence(Sequence animationSequence)
         {
             wasActive = targetGameObject.activeSelf;
-
             Sequence behaviourSequence = DOTween.Sequence();
             behaviourSequence.SetDelay(Delay);
 
